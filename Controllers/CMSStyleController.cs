186920eee85d1e161c98c416daa9675a7bd95eb8
murali krishnan <murali@shss.co.in>
2024-05-01 14:22:41 +0530

    cms style controller update

---
+++ Controllers/CMSStyleController.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationDbContext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeveloperAssessmentProject.Controllers
+{
+    public class CMSStyleController : Controller
+    {
+        private readonly AppDbContext _Context;
+
+        public CMSStyleController(AppDbContext context)
+        {
+            _Context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _Context.Books.ToList();
+
+            return View(list);
+        }
+    }
+}
